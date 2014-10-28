86664107f7285117e79b57ae7c5479dad489630d
Ian Michell <ian@ians-macbook.home>
2014-10-28 20:50:34 +0000

    added a bit more to mapped file store

--- core/src/main/java/com/pungwe/db/io/MemoryMappedFileStore.java
+++ core/src/main/java/com/pungwe/db/io/MemoryMappedFileStore.java
@@ -19,12 +19,16 @@
 package com.pungwe.db.io;
 
 import com.pungwe.db.io.serializers.Serializer;
+import com.pungwe.db.types.DBObject;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by ian on 28/10/2014.
@@ -32,12 +36,47 @@ import java.util.List;
 public class MemoryMappedFileStore implements Store {
 
 	// We don't want anymore than 2GB per segment...
-	private static final int MAX_SEGMENT_SIZE = Integer.MAX_VALUE;
+	private static final int MAX_SEGMENTS = Integer.MAX_VALUE;
+	private static final int PAGE_SIZE = 4096; // 4KB blocks of data.
 
-	private List<MappedByteBuffer> segments = new ArrayList<MappedByteBuffer>();
+	private List<MappedByteBuffer> segments = new ArrayList<>();
+
+	private RandomAccessFile file;
+	private long length;
+	private final long maxLength;
+	private final AtomicLong nextPosition = new AtomicLong();
+
+	public MemoryMappedFileStore(File file, long initialSize, long maxFileSize) throws IOException {
+		this.file = new RandomAccessFile(file, "rw");
+		if (this.file.length() == 0) {
+			this.file.setLength(initialSize);
+			this.nextPosition.set(0); // Set to the top of the file
+		} else {
+			// Find the end of the file
+		}
+		long segCount = initialSize / MAX_SEGMENTS;
+		if (segCount > MAX_SEGMENTS) {
+			throw new ArithmeticException("Requested File Size is too large");
+		}
+		length = initialSize;
+		maxLength = maxFileSize;
+		long countDown = initialSize;
+		long from = 0;
+		FileChannel channel = this.file.getChannel();
+		while (countDown > 0) {
+			long len = Math.min(MAX_SEGMENTS, countDown);
+			MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_WRITE, from, len);
+			segments.add(buffer);
+			from += len;
+			countDown -= len;
+		}
+	}
 
 	@Override
 	public <T> long put(T value, Serializer<T> serializer) throws IOException {
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		DataOutputStream out = new DataOutputStream(bytes);
+
 		return 0;
 	}
 
